3ffb093ff88f59a5e8dc2b2b770a1dfdf8a1bb84
Nadrieril <nadrieril+git@gmail.com>
2024-12-22 16:10:38 +0100

    Improve the derive macro code

--- derive_generic_visitor_macros/src/drive.rs
+++ derive_generic_visitor_macros/src/drive.rs
@@ -1,72 +1,52 @@
+use darling::ast::{Data, Fields};
 use darling::{FromDeriveInput, FromField, FromVariant};
-use itertools::Itertools;
 use proc_macro2::{Span, TokenStream};
 use quote::{quote, ToTokens};
-use std::iter::IntoIterator;
-use syn::token::Mut;
-use syn::{
-    parse_quote, Data, DeriveInput, Error, Field, GenericParam, Ident, Index, Lifetime, Path,
-    Result, WhereClause,
-};
+use syn::{parse_quote, DeriveInput, GenericParam, Generics, Ident, Index, Path, Result, Type};
+
+use crate::Names;
 
 #[derive(FromDeriveInput)]
 #[darling(attributes(drive))]
-struct TypeAttrs {
+struct MyTypeDecl {
+    ident: Ident,
+    generics: Generics,
+    data: Data<MyVariant, MyField>,
     skip: Option<()>,
 }
 
 #[derive(FromVariant)]
 #[darling(attributes(drive))]
-struct VariantAttrs {
+struct MyVariant {
+    ident: Ident,
+    fields: Fields<MyField>,
     skip: Option<()>,
 }
 
 #[derive(FromField)]
 #[darling(attributes(drive))]
-struct FieldAttrs {
+struct MyField {
+    ident: Option<Ident>,
+    ty: Type,
     skip: Option<()>,
 }
 
-struct Ctx<'a> {
-    visit_trait: &'a Path,
-    visitor_param: &'a Ident,
-    lifetime_param: &'a Lifetime,
-    where_clause: &'a mut WhereClause,
-}
-
 pub fn impl_drive(input: DeriveInput, mutable: bool) -> Result<TokenStream> {
-    let attrs = TypeAttrs::from_derive_input(&input)?;
+    let names = Names::new(mutable);
+    let Names {
+        visitor_trait,
+        visit_trait,
+        drive_trait,
+        drive_method,
+        visitor_param,
+        lifetime_param,
+        mut_modifier,
+        ..
+    } = &names;
 
-    let crate_path: Path = parse_quote! { ::derive_generic_visitor };
-    let visitor_trait: Path = parse_quote!( #crate_path::Visitor );
-    let visit_trait: Path = if mutable {
-        parse_quote!( #crate_path::VisitMut )
-    } else {
-        parse_quote!( #crate_path::Visit )
-    };
-    let drive_trait: Path = if mutable {
-        parse_quote!( #crate_path::DriveMut )
-    } else {
-        parse_quote!( #crate_path::Drive )
-    };
-    let method = Ident::new(
-        if mutable {
-            "drive_inner_mut"
-        } else {
-            "drive_inner"
-        },
-        Span::call_site(),
-    );
+    let input = MyTypeDecl::from_derive_input(&input)?;
 
-    let visitor_param = Ident::new("V", Span::call_site());
-    let lifetime_param: Lifetime = parse_quote!('s);
-    let mut_modifier = if mutable {
-        Some(Mut(Span::call_site()))
-    } else {
-        None
-    };
-
-    let name = input.ident;
+    let name = &input.ident;
     let (_, ty_generics, _) = input.generics.split_for_impl();
     let impl_subject = quote! { #name #ty_generics };
 
@@ -77,42 +57,38 @@ pub fn impl_drive(input: DeriveInput, mutable: bool) -> Result<TokenStream> {
     generics
         .params
         .push(GenericParam::Type(parse_quote!(#visitor_param)));
-    // We will add `V: Visit<'s, FieldTy>` clauses for each field.
+
     let where_clause = generics.make_where_clause();
     // Add `V: Visitor` so we can name `V::Break` even for a unit struct.
     where_clause
         .predicates
         .push(parse_quote!(#visitor_param: #visitor_trait));
-
-    let mut ctx = Ctx {
-        visit_trait: &visit_trait,
-        visitor_param: &visitor_param,
-        lifetime_param: &lifetime_param,
-        where_clause,
+    // Adds a `V: Visit<'s, FieldTy>` clause for each field.
+    let mut need_visit_type = |f: &MyField| {
+        let field_ty = &f.ty;
+        where_clause
+            .predicates
+            .push(parse_quote!(#visitor_param: #visit_trait<#lifetime_param, #field_ty>));
     };
+
     let arms = match input.data {
-        _ if attrs.skip.is_some() => quote!(),
-        Data::Struct(struct_) => {
-            match_variant(&mut ctx, &parse_quote!(Self), struct_.fields.iter())?
+        _ if input.skip.is_some() => quote!(),
+        Data::Struct(fields) => {
+            match_variant(&names, parse_quote!(Self), fields.iter(), need_visit_type)
         }
-        Data::Enum(enum_) => enum_
-            .variants
-            .into_iter()
-            .map(|x| {
-                let attrs = VariantAttrs::from_variant(&x)?;
-                if attrs.skip.is_some() {
-                    return Ok(TokenStream::new());
-                }
-                let name = x.ident;
-                match_variant(&mut ctx, &parse_quote!(Self::#name), x.fields.iter())
+        Data::Enum(variants) => variants
+            .iter()
+            .filter(|variant| variant.skip.is_none())
+            .map(|variant| {
+                let name = &variant.ident;
+                match_variant(
+                    &names,
+                    parse_quote!(Self::#name),
+                    variant.fields.iter(),
+                    &mut need_visit_type,
+                )
             })
-            .try_collect()?,
-        Data::Union(union_) => {
-            return Err(Error::new_spanned(
-                union_.union_token,
-                "unions are not supported",
-            ));
-        }
+            .collect(),
     };
 
     let (impl_generics, _, where_clause) = generics.split_for_impl();
@@ -120,7 +96,7 @@ pub fn impl_drive(input: DeriveInput, mutable: bool) -> Result<TokenStream> {
         impl #impl_generics #drive_trait<#lifetime_param, #visitor_param> for #impl_subject
         #where_clause {
             #[allow(non_shorthand_field_patterns, unused_variables)]
-            fn #method(&#lifetime_param #mut_modifier self, visitor: &mut #visitor_param)
+            fn #drive_method(&#lifetime_param #mut_modifier self, visitor: &mut #visitor_param)
                     -> ::std::ops::ControlFlow<#visitor_param::Break> {
                 match self {
                     #arms
@@ -135,13 +111,20 @@ pub fn impl_drive(input: DeriveInput, mutable: bool) -> Result<TokenStream> {
 /// Generate a match arm that destructures the fields of the given variant and visits each of these
 /// fields.
 fn match_variant<'a>(
-    ctx: &mut Ctx<'_>,
-    name: &Path,
-    fields: impl Iterator<Item = &'a Field>,
-) -> Result<TokenStream> {
+    names: &Names,
+    name: Path,
+    fields: impl Iterator<Item = &'a MyField>,
+    mut for_each_field: impl FnMut(&'a MyField),
+) -> TokenStream {
+    let visitor_param = &names.visitor_param;
+    let visit_trait = &names.visit_trait;
     let (destructuring, visit_fields): (TokenStream, TokenStream) = fields
         .enumerate()
+        .filter(|(_, field)| field.skip.is_none())
         .map(|(index, field)| {
+            // Add a where clause to ensure this type can be visited.
+            for_each_field(field);
+            let field_ty = &field.ty;
             let field_id: TokenStream = match &field.ident {
                 None => Index::from(index).into_token_stream(),
                 Some(name) => name.into_token_stream(),
@@ -150,35 +133,17 @@ fn match_variant<'a>(
                 None => Ident::new(&format!("i{}", index), Span::call_site()).into_token_stream(),
                 Some(name) => name.into_token_stream(),
             };
-            let field_pat = quote!( #field_id : #var, );
-            let visit_field = visit_field(ctx, &var, field)?;
-            Ok((field_pat, visit_field))
+            (
+                // Destructure this field
+                quote!( #field_id : #var, ),
+                // Call `visitor.visit()` on the field.
+                quote!( <#visitor_param as #visit_trait<#field_ty>>::visit(visitor, #var); ),
+            )
         })
-        .try_collect::<_, _, Error>()?;
-    Ok(quote! {
-        #name { #destructuring } => {
+        .collect();
+    quote! {
+        #name { #destructuring .. } => {
             #visit_fields
         }
-    })
-}
-
-/// Visit a single field by calling `visitor.visit()` on it. Also adds a where clause to the impl
-/// to that this call is valid.
-fn visit_field(ctx: &mut Ctx<'_>, value_expr: &TokenStream, field: &Field) -> Result<TokenStream> {
-    let attrs = FieldAttrs::from_field(&field)?;
-    if attrs.skip.is_some() {
-        return Ok(TokenStream::new());
     }
-
-    let visitor_param = ctx.visitor_param;
-    let lifetime_param = ctx.lifetime_param;
-    let visit_trait = ctx.visit_trait;
-    let field_ty = &field.ty;
-    ctx.where_clause
-        .predicates
-        .push(parse_quote!(#visitor_param: #visit_trait<#lifetime_param, #field_ty>));
-
-    Ok(quote! {
-        <#visitor_param as #visit_trait<#field_ty>>::visit(visitor, #value_expr);
-    })
 }
--- derive_generic_visitor_macros/src/lib.rs
+++ derive_generic_visitor_macros/src/lib.rs
@@ -1,21 +1,63 @@
 //! Derive macros for the `Drive`/`DriveMut` traits in `derive_generic_visitor`.
-use syn::parse_macro_input;
-use syn::DeriveInput;
+use proc_macro2::TokenStream;
+use syn::*;
+use token::Mut;
 
 mod drive;
 
-#[proc_macro_derive(Drive, attributes(drive))]
-pub fn derive_drive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+fn expand_with(
+    input: proc_macro::TokenStream,
+    handler: impl Fn(DeriveInput) -> Result<TokenStream>,
+) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    drive::impl_drive(input, false)
+    handler(input)
         .unwrap_or_else(|error| error.to_compile_error())
         .into()
 }
 
+/// Shared logic to get the important paths and identifiers for this crate.
+struct Names {
+    visitor_trait: Path,
+    visit_trait: Path,
+    drive_trait: Path,
+    drive_method: Ident,
+    visitor_param: Ident,
+    lifetime_param: Lifetime,
+    mut_modifier: Option<Mut>,
+}
+impl Names {
+    fn new(mutable: bool) -> Names {
+        let crate_path: Path = parse_quote! { ::derive_generic_visitor };
+        Names {
+            visitor_trait: parse_quote!( #crate_path::Visitor ),
+            visit_trait: if mutable {
+                parse_quote!( #crate_path::VisitMut )
+            } else {
+                parse_quote!( #crate_path::Visit )
+            },
+            drive_trait: if mutable {
+                parse_quote!( #crate_path::DriveMut )
+            } else {
+                parse_quote!( #crate_path::Drive )
+            },
+            drive_method: if mutable {
+                parse_quote!(drive_inner_mut)
+            } else {
+                parse_quote!(drive_inner)
+            },
+            visitor_param: parse_quote!(V),
+            lifetime_param: parse_quote!('s),
+            mut_modifier: mutable.then(Default::default),
+        }
+    }
+}
+
+#[proc_macro_derive(Drive, attributes(drive))]
+pub fn derive_drive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    expand_with(input, |input| drive::impl_drive(input, false))
+}
+
 #[proc_macro_derive(DriveMut, attributes(drive))]
 pub fn derive_drive_mut(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
-    drive::impl_drive(input, true)
-        .unwrap_or_else(|error| error.to_compile_error())
-        .into()
+    expand_with(input, |input| drive::impl_drive(input, true))
 }
