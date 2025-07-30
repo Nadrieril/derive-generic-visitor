e8f75ac2d5aa372e0a0e28cf1486b92b2b49c93c
Lucas Franceschino <lucas@cryspen.com>
2025-07-30 14:49:38 +0200

    feat: inherit doc from `README.md`

---
+++ derive_generic_visitor/build.rs
@@ -0,0 +1,8 @@
+use std::{env, fs, path::PathBuf};
+
+fn main() {
+    let readme = fs::read_to_string("../README.md").unwrap();
+    let doc = readme.split_once("<!-- start-doc -->").unwrap().1.trim();
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    fs::write(out_dir.join("docs.md"), doc).unwrap();
+}
